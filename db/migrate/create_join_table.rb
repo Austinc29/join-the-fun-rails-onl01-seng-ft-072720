f5a6fef9b43d42077b26f8fd3733dfc06b7a8901
Austin Cummings <aac729@live.com>
2020-09-30 18:06:24 +0000

    Automatically backed up by Learn

--- db/migrate/create_join_table.rb
+++ db/migrate/create_join_table.rb
@@ -1,5 +1,6 @@
-
-create_join_table :events, :users do |t|
-  t.index :event_id
-  t.index :user_id
-end
+class CreateJoinTable > ActiveRecord::Base 
+  create_join_table :events, :users do |t|
+    t.index :event_id
+    t.index :user_id
+  end
+end 
